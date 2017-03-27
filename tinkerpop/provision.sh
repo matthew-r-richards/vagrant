6a40a733d36a5682759f8c95c68bbf88cf9e4a29
Matthew Richards <matthew.richards@zuhlke.com>
2017-03-27 10:48:56 +0100

    Add Cassandra installation (WIP)

--- tinkerpop/provision.sh
+++ tinkerpop/provision.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-# Install Java
+# Add APT repositories
 sudo add-apt-repository -y ppa:openjdk-r/ppa
+echo "deb http://www.apache.org/dist/cassandra/debian 36x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+curl -s https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
+
 sudo apt-get update
+
+# Install Java etc.
 sudo apt-get -y install openjdk-8-jdk maven git
 
-# Place zip file in /vagrant shared folder if you want to avoid the clone+make process
+# Install Cassandra
+sudo apt-get -y install cassandra
+
+# Install JanusGraph - two options:
+# 1. Place zip file in /vagrant shared folder if you want to avoid the clone+build process
 #cp /vagrant/janusgraph-0.1.0-SNAPSHOT-hadoop2.zip ~/janusgraph-0.1.0-SNAPSHOT-hadoop2.zip
 
-# Install JanusGraph
+# 2. Clone Git repo and build
 git clone https://github.com/JanusGraph/janusgraph.git ~/janusgraph-src
 cd ~/janusgraph-src
 mvn clean install -Pjanusgraph-release -Dgpg.skip=true -DskipTests=true
